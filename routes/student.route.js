5d8ea9313bafad367f83b511de3849682c2f5f92
darshil-nathani <darshilnathani1412@gmail.com>
2024-07-16 05:49:29 -0700

    update delete

--- routes/student.route.js
+++ routes/student.route.js
@@ -5,7 +5,8 @@ const controller = require('../controllers/student.controller')
 
 router.get("/get",controller.getAllStudent);
 router.post("/create",controller.createStudent);
-
+router.delete("/delete/:studentId",controller.deleteStudent);
+router.put("/delete/:studentId",controller.updateStudent);
 
 
 
